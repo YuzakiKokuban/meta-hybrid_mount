2f9bbc1d4a2a6e34056cbf9269aee624bbf32269
知秋 <127842603+947409161@users.noreply.github.com>
2025-12-28 07:27:24 +0800

    Delete module/uninstall.sh

--- module/uninstall.sh
+++
@@ -1,8 +0,0 @@
-
-BASE_DIR="/data/adb/meta-hybrid"
-MNT_DIR="$BASE_DIR/mnt"
-if mountpoint -q "$MNT_DIR"; then
-    umount "$MNT_DIR" 2>/dev/null || umount -l "$MNT_DIR"
-fi
-rm -rf "$BASE_DIR"
-exit 0
